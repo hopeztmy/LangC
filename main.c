7007c75076f753abea4bf49fce6f43056620ee7e
yoshida nozomu <non3964.ztmy@gmail.com>
2023-11-24 13:16:04 +0900

    Merge branch 'main' of https://github.com/hopeztmy/LangC

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-    printf("Hello world\n");
+    printf("Welcome to C Programming!\n");
+
+    int x = 10;
+    int y = x * x;
+    int z = x + y;
+    printf("x = %d, y = %d, z = %d\n", x, y, z);
+
+    int price = 500;
+    printf("%.1f\n", price * 1.1);
     return 0;
 }
